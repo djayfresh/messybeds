d891d7dc9f3254d27ec6e41eff6e3cc44093ce5a
djayfresh <douglasjayfresh@gmail.com>
2022-04-08 09:12:11 -0500

    Mod config and class setup method

--- src/main/java/com/djayfresh/messybeds/MessyBeds.java
+++ src/main/java/com/djayfresh/messybeds/MessyBeds.java
@@ -1,5 +1,32 @@
 package com.djayfresh.messybeds;
 
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.slf4j.Logger;
+
+@Mod("messybeds")
 public class MessyBeds {
+    private static final Logger LOGGER = LogUtils.getLogger();
     
+    public MessyBeds()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("MESSY BEDS >> {}", Blocks.WHITE_BED.getRegistryName());
+    }
 }
