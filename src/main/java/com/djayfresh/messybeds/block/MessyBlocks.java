e45f520970fb75ec3c7f168c8030b7ecf6d28849
djayfresh <douglasjayfresh@gmail.com>
2022-04-19 19:27:59 -0500

    All beds rendering and lang

--- src/main/java/com/djayfresh/messybeds/block/MessyBlocks.java
+++ src/main/java/com/djayfresh/messybeds/block/MessyBlocks.java
@@ -20,14 +20,40 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class MessyBlocks {
 
-    public static final MessyBedBlock WHITE_BED = MessyBlocks.buildMessyBed(DyeColor.WHITE);
-    public static final MessyBedBlock RED_BED = MessyBlocks.buildMessyBed(DyeColor.RED);
+    public static final MessyBedBlock BLACK_BED = MessyBlocks.buildMessyBed(DyeColor.BLACK);
+    public static final MessyBedBlock BLUE_BED = MessyBlocks.buildMessyBed(DyeColor.BLUE);
+    public static final MessyBedBlock BROWN_BED = MessyBlocks.buildMessyBed(DyeColor.BROWN);
+    public static final MessyBedBlock CYAN_BED = MessyBlocks.buildMessyBed(DyeColor.CYAN);
+    public static final MessyBedBlock GRAY_BED = MessyBlocks.buildMessyBed(DyeColor.GRAY);
+    public static final MessyBedBlock GREEN_BED = MessyBlocks.buildMessyBed(DyeColor.GREEN);
+    public static final MessyBedBlock LIGHT_BLUE_BED = MessyBlocks.buildMessyBed(DyeColor.LIGHT_BLUE);
+    public static final MessyBedBlock LIGHT_GRAY_BED = MessyBlocks.buildMessyBed(DyeColor.LIGHT_GRAY);
+    public static final MessyBedBlock LIME_BED = MessyBlocks.buildMessyBed(DyeColor.LIME);
+    public static final MessyBedBlock MAGENTA_BED = MessyBlocks.buildMessyBed(DyeColor.MAGENTA);
     public static final MessyBedBlock ORANGE_BED = MessyBlocks.buildMessyBed(DyeColor.ORANGE);
+    public static final MessyBedBlock PINK_BED = MessyBlocks.buildMessyBed(DyeColor.PINK);
+    public static final MessyBedBlock PURPLE_BED = MessyBlocks.buildMessyBed(DyeColor.PURPLE);
+    public static final MessyBedBlock RED_BED = MessyBlocks.buildMessyBed(DyeColor.RED);
+    public static final MessyBedBlock WHITE_BED = MessyBlocks.buildMessyBed(DyeColor.WHITE);
+    public static final MessyBedBlock YELLOW_BED = MessyBlocks.buildMessyBed(DyeColor.YELLOW);
 
     public static void registerAll(IForgeRegistry<Block> registry) {
-        registry.register(setup(MessyBlocks.WHITE_BED, "white_bed", MessyItems.TAB_MESSY_ITEMS));
-        registry.register(setup(MessyBlocks.RED_BED, "red_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.BLACK_BED, "black_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.BLUE_BED, "blue_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.BROWN_BED, "brown_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.CYAN_BED, "cyan_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.GRAY_BED, "gray_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.GREEN_BED, "green_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.LIGHT_BLUE_BED, "light_blue_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.LIGHT_GRAY_BED, "light_gray_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.LIME_BED, "lime_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.MAGENTA_BED, "magenta_bed", MessyItems.TAB_MESSY_ITEMS));
         registry.register(setup(MessyBlocks.ORANGE_BED, "orange_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.PINK_BED, "pink_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.PURPLE_BED, "purple_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.RED_BED, "red_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.WHITE_BED, "white_bed", MessyItems.TAB_MESSY_ITEMS));
+        registry.register(setup(MessyBlocks.YELLOW_BED, "yellow_bed", MessyItems.TAB_MESSY_ITEMS));
     }
 
     private static MessyBedBlock buildMessyBed(DyeColor color) {
--- src/main/java/com/djayfresh/messybeds/block/entity/MessyEntities.java
+++ src/main/java/com/djayfresh/messybeds/block/entity/MessyEntities.java
@@ -12,7 +12,24 @@ import net.minecraftforge.registries.RegistryObject;
 public class MessyEntities {
     private static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MessyBeds.MOD_ID);
     
-    public static final RegistryObject<BlockEntityType<MessyBedEntity>> BED = BLOCK_ENTITY_REGISTER.register("messybed", () -> BlockEntityType.Builder.of(MessyBedEntity::new, MessyBlocks.RED_BED, MessyBlocks.WHITE_BED, MessyBlocks.ORANGE_BED).build(null));
+    public static final RegistryObject<BlockEntityType<MessyBedEntity>> BED = BLOCK_ENTITY_REGISTER.register("messybed", () -> BlockEntityType.Builder.of(MessyBedEntity::new, 
+        MessyBlocks.BLACK_BED,
+        MessyBlocks.BLUE_BED,
+        MessyBlocks.BROWN_BED,
+        MessyBlocks.CYAN_BED,
+        MessyBlocks.GRAY_BED,
+        MessyBlocks.GREEN_BED,
+        MessyBlocks.LIGHT_BLUE_BED,
+        MessyBlocks.LIGHT_GRAY_BED,
+        MessyBlocks.LIME_BED,
+        MessyBlocks.MAGENTA_BED,
+        MessyBlocks.ORANGE_BED,
+        MessyBlocks.PINK_BED,
+        MessyBlocks.PURPLE_BED,
+        MessyBlocks.RED_BED,
+        MessyBlocks.WHITE_BED,
+        MessyBlocks.YELLOW_BED
+    ).build(null));
 
     public static void registerBus(IEventBus bus){
         BLOCK_ENTITY_REGISTER.register(bus);
